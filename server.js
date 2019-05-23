633e9fb7e455193b31e7bec29b1e34390b73c1af
Umeh Nnamdi Kingsley <endeking@gmail.com>
2019-05-23 07:08:39 +0100

    fixed server js error

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 // ^ middleware to redirect all URLs to index.html
 
-const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname))
+const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname));
 
 app.use(staticFileMiddleware);
 app.use(history());
@@ -13,8 +13,6 @@ app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/index.html'))
-})
+});
 
-app.listen(5000, function () {
-    console.log( 'Express serving on 5000!' )
-})
+app.listen(process.env.PORT || 5000);
